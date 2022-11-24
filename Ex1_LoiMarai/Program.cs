fe1042db5ab2ed91f740a700cbcae5d4279a9410
unknown <pongsapat.inta@kmutt.acth>
2022-11-24 22:51:11 +0700

    Edit Code Homework 1

--- Ex1_LoiMarai/Program.cs
+++ Ex1_LoiMarai/Program.cs
@@ -2,11 +2,44 @@
 {
     static void Main(string[] args)
     {
-        Queue<int>LoiMarai = new Queue<int>{};
+        Queue<char>LoiMarai = new Queue<char>{};
         char NameFlowers = '?';
-        while(NameFlowers == '?'||NameFlowers == 'J'){
+        int NubG = 0;
+        
+        
+        while(NameFlowers == '?'||NameFlowers == 'J'||NameFlowers == 'G'||NameFlowers == 'O'||NameFlowers == 'R'){
+            NameFlowers = char.Parse(Console.ReadLine());
+
+                if(NameFlowers == 'J'){
+                    LoiMarai.Push('J');
+                    
+                }
+                else if (NameFlowers == 'G'){   
+                    if(NubG <3){
+                        LoiMarai.Push('G');
+                        NubG = NubG + 1 ;
+                    }
+                    else{
+                        Console.WriteLine("Invalid pattern.");
+                    }
+                }
+                else if (NameFlowers == 'O'){
+                    LoiMarai.Push('O');
+                }
+                else if (NameFlowers == 'R'){
+                    LoiMarai.Push('R');
+                }
                 
         }
+        ShowFlower(LoiMarai);
+        
+    static void ShowFlower(Queue<char>LoiMarai){
+        for(int i = 0; i < LoiMarai.GetLength(); i++)
+        {
+            Console.Write("{0}",LoiMarai.Get(i));
+        }
+        
+    }
 
     }
 }
