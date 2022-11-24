c4c26cdf3cddaf1c059ab6c5398b3837b8300971
unknown <pongsapat.inta@kmutt.acth>
2022-11-24 22:26:12 +0700

    Edit Code Homework2

---
+++ Ex1_LoiMarai/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<int>LoiMarai = new Queue<int>{};
+        char NameFlowers = '?';
+        while(NameFlowers == '?'||NameFlowers == 'J'){
+                
+        }
+
+    }
+}
